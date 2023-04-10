d060fc6c489e6384b8836e3740f44717ca640f35
ALFREDO ENRIQUE ALVAREZ CASTRO <130337120+AlvarezICJ@users.noreply.github.com>
2023-04-10 01:45:12 -0500

    Update ventas_diarias.py

--- ventas_diarias.py
+++ ventas_diarias.py
@@ -1,46 +1,31 @@
 import pandas as pd
-from twilio.rest import Client
 from datetime import datetime
+from twilio.rest import Client
 
 # Configuración de Twilio
-account_sid = 'TU_ACCOUNT_SID'
-auth_token = 'TU_AUTH_TOKEN'
+account_sid = 'your_account_sid_here'
+auth_token = 'your_auth_token_here'
 client = Client(account_sid, auth_token)
-twilio_number = 'TU_NUMERO_DE_TWILIO'
-my_number = 'TU_NUMERO_DE_TELEFONO'
+my_twilio_number = 'your_twilio_number_here'
+my_cell_number = 'your_cell_number_here'
 
-# Ruta y nombre del archivo de Excel
-file_name = 'ventas_diarias.xlsx'
+# Obtener fecha y hora actual
+now = datetime.now()
+fecha = now.strftime("%d/%m/%Y")
+hora = now.strftime("%H:%M:%S")
 
-# Carga los datos del archivo de Excel en un DataFrame de pandas
-try:
-    df = pd.read_excel(file_name)
-except:
-    df = pd.DataFrame(columns=['Fecha', 'Paquete', 'Ventas', 'Total'])
+# Solicitar información de la venta
+nombre = input("Ingresa el nmbre del paquete: ")
+paquete = input("Ingrese el tipo de paquete vendido: ")
+venta = int(input("Ingrese la cantidad vendida: "))
 
-# Obtiene la fecha, el nombre del paquete y las ventas del día
-fecha = datetime.now().strftime('%Y-%m-%d')
-paquete = input('Ingrese el nombre del paquete vendido hoy: ')
-ventas = int(input('Ingrese la cantidad de paquetes premium vendidos hoy: '))
+# Guardar la venta en el archivo de Excel
+nueva_venta = {"Fecha": fecha, "Hora": hora, "Paquete": paquete, "Venta": venta}
+df = pd.DataFrame([nueva_venta])
+df.to_excel(r'ventas.xlsx', index=None, header=True)
 
-# Pregunta si se vendió en oferta
-oferta = input('¿Se vendió en oferta a 19900 COP? (s/n) ')
+# Enviar mensaje de texto con información de la venta
+mensaje = f"Se ha vendido {venta} paquete(s) {paquete} de {nombre} el día de hoy."
+message = client.messages.create(body=mensaje, from_=my_twilio_number, to=my_cell_number)
+print("Mensaje enviado correctamente:", message.sid)
 
-if oferta.lower() == 's':
-    precio_unitario = 19900
-else:
-    precio_unitario = 25000
-
-# Calcula el total de las ventas
-total = ventas * precio_unitario
-
-# Agrega los datos de ventas al DataFrame
-row = {'Fecha': fecha, 'Paquete': paquete, 'Ventas': ventas, 'Total': total}
-df = df.append(row, ignore_index=True)
-
-# Guarda el DataFrame en el archivo de Excel
-df.to_excel(file_name, index=False)
-
-# Envía un mensaje de texto con los datos de ventas
-message = f'Hoy ({fecha}) se vendieron {ventas} paquetes {paquete}, con un total de ${total}.'
-client.messages.create(to=my_number, from_=twilio_number, body=message)
